1922d41bcde3940a7d643e5940666be335a313fc
000carla000 <61315374+000carla000@users.noreply.github.com>
2020-02-21 13:22:26 +0100

    Add files via upload

---
+++ CSS1[1].php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p {
+  color: blue;
+  /* This is a single-line comment */
+  text-align: left
+;
+} 
+
+/* This is
+a multi-line
+comment */
+</style>
+</head>
+<body>
+
+<p>Hello World!</p>
+<p>This paragraph is styled with CSS.</p>
+<p>CSS comments are not shown in the output.</p>
+
+</body>
+</html>
